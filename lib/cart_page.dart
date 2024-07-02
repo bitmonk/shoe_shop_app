0e7a554048d757cff7a0e1a7109e9b58b0613261
Parbat Tamang <parbattamang1234@gmail.com>
2024-07-02 16:17:59 +0545

    add delete button in cart

---
+++ lib/cart_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app_flutter/global_variables.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cart'),
+      ),
+      body: ListView.builder(
+        itemCount: cart.length,
+        itemBuilder: (context, index) {
+          final cartItem = cart[index];
+
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage(cartItem['imageUrl'] as String),
+              radius: 30,
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.delete,
+                color: Color.fromARGB(255, 182, 46, 37),
+              ),
+            ),
+            title: Text(
+              cartItem['title'].toString(),
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+            subtitle: Text('Size : ${cartItem['size']}'),
+          );
+        },
+      ),
+    );
+  }
+}
