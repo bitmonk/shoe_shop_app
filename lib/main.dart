892a1b1daacf1a30bdecbd182d30465a0e13eae1
Parbat Tamang <parbattamang1234@gmail.com>
2024-07-02 15:08:09 +0545

    product details page add

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app_flutter/global_variables.dart';
 import 'package:shop_app_flutter/home_page.dart';
-import 'package:shop_app_flutter/product_details_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,10 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Shopping App',
-      home: ProductDetailsPage(
-        product: products[0],
-      ),
+      home: const HomePage(),
       theme: ThemeData(
           fontFamily: 'Lato',
           colorScheme: ColorScheme.fromSeed(
--- lib/product_details_page.dart
+++ lib/product_details_page.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
-class ProductDetailsPage extends StatelessWidget {
+class ProductDetailsPage extends StatefulWidget {
   final Map<String, Object> product;
   const ProductDetailsPage({
     super.key,
     required this.product,
   });
 
+  @override
+  State<ProductDetailsPage> createState() => _ProductDetailsPageState();
+}
+
+class _ProductDetailsPageState extends State<ProductDetailsPage> {
+  int selectedSize = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +22,91 @@ class ProductDetailsPage extends StatelessWidget {
       body: Column(
         children: [
           Text(
-            product['title'] as String,
+            widget.product['title'] as String,
             style: Theme.of(context).textTheme.titleLarge,
-          )
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(widget.product['imageUrl'] as String),
+          ),
+          const Spacer(
+            flex: 2,
+          ),
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(245, 247, 249, 1),
+              borderRadius: BorderRadius.circular(40),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '\$${widget.product['price']}',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  height: 50,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: (widget.product['sizes'] as List<int>).length,
+                    itemBuilder: (context, index) {
+                      final size =
+                          (widget.product['sizes'] as List<int>)[index];
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selectedSize = size;
+                            });
+                          },
+                          child: Chip(
+                            label: Text(size.toString()),
+                            backgroundColor: selectedSize == size
+                                ? Theme.of(context).colorScheme.primary
+                                : null,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      minimumSize: const Size(double.infinity, 50),
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment
+                          .center, // Center the content within the button
+                      children: [
+                        Icon(Icons.add_shopping_cart,
+                            color: Colors.black), // Add your desired icon here
+                        SizedBox(
+                            width:
+                                8), // Add some spacing between the icon and the text
+                        Text(
+                          'Add To Cart',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
